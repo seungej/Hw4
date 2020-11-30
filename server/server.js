0ce3b1d05acb2f95191dfe6779866a5d53c23593
Seung <seungej@gmail.com>
2020-11-30 10:48:30 -0600

    Updated

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
-
 const morgan = require("morgan");
-
 const app = express();
 
 app.use(cors());
