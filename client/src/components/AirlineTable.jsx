57c33fd75a8b82589fa6bef7c3060fe5d349b3c1
Julian Aguilar <julianaguilar98@gmail.com>
2020-12-03 15:34:59 -0600

    added table to front page

--- client/src/components/AirlineTable.jsx
+++ client/src/components/AirlineTable.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useContext} from 'react'
 import AirportFinder from "../apis/AirportFinder";
 import { AirlineContext } from "../context/AirlineContext";
 
 const AirlineTable = (props) => {
-	const {airlines, setAirline} = useContext(AirlineContext)
+	const {airlines, setAirline} = useContext(AirlineContext);
 	useEffect(() => {
 		const fetchTableData = async() => {
 			try{
 				const tableResponse = await AirportFinder.get("/");
-				console.log(tableResponse);
+				setAirline(tableResponse.data.data.airlines)
 			} catch(err){}
 		};
 		
@@ -18,12 +18,39 @@ const AirlineTable = (props) => {
 	return(
 		<div className="list-group">
 			<table className="table table-hover table-dark">
+			  <thead>
+				<tr className="bg-secondary">
+					<th scope="col">Flight#</th>
+					<th scope="col">From</th>
+					<th scope="col">To</th>
+					<th scope="col">Departure</th>
+					<th scope="col">Arrival</th>
+					<th scope="col">Seats Available</th>
+					<th scope="col">Book</th>
+				</tr>
+			  </thead>
 				<tbody>
-				
+					{airlines && airlines.map((airline) => {
+						return(
+						<tr key={airline.flight_id}>
+							<td>{airline.flight_no}</td>
+							<td>{airline.departure_airport}</td>
+							<td>{airline.arrival_airport}</td>
+							<td>{airline.scheduled_departure}</td>
+							<td>{airline.scheduled_arrival}</td>
+							<td>{airline.seats_available}</td>
+							<td>
+							    <button className="btn btn-secondary">
+								Book
+								</button>
+							</td>
+						</tr>
+						);
+					})}
 				</tbody>
 			</table>
 		</div>
-	)
-}
+	);
+};
 
-export default AirlineTable
+export default AirlineTable;
